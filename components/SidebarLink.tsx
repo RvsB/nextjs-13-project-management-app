927ef39baa9442e3c0feb3f95ecfc5652c1608b4
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-07 01:23:31 +0530

    FEAT: minor change to the SidebarLink component

--- components/SidebarLink.tsx
+++ components/SidebarLink.tsx
@@ -6,11 +6,11 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
 const icons = { Settings, User, Grid, Calendar }; //a map of icons, we need this because a server component will be passing down the props to this client component and when passing down the props they will come through a network layer, and only serializable things are able to pass through it, and since functions arent serializable (nor date and time types), components are also functions, hence we cant send them as props, so we pass a string instead, so when this client components gets rendered, we check the Icons object for the string we got, and get the icon component
-
 interface Props {
   link: {
-    icon: "Settings" | "User" | "Grid" | "Calendar";
-    [key: string]: any;
+    label: string;
+    icon: keyof typeof icons;
+    link: string;
   };
 }
 
