51839b886c1c001100f044fbf3dbe70edb6de402
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-14 00:59:07 +0530

    FEAT: added a generic fetcher utility for register and signin and in general

---
+++ lib/api.ts
@@ -0,0 +1,48 @@
+interface FetcherProps {
+  url: string;
+  method: string;
+  body: { [key: string]: any };
+  json: boolean;
+}
+
+interface UserObj {
+  [key: string]: any;
+}
+
+const fetcher = async ({ url, method, body, json = true }: FetcherProps) => {
+  const res = await fetch(url, {
+    method,
+    body: body && JSON.stringify(body),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("API Error");
+  }
+
+  if (json) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const register = async (user: UserObj) => {
+  return fetcher({
+    url: "/api/register",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
+
+export const signin = async (user: UserObj) => {
+  return fetcher({
+    url: "/api/signin",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
