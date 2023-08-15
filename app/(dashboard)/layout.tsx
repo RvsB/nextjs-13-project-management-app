b1028fffdb564fd6b8cc168cbb1342b37fd557a7
Raj Vardhan Singh Birani <rajvardhan.singh@tmdc.io>
2023-08-16 00:45:19 +0530

    FEAT: added the sidebar in the dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/global.css";
 
 import GlassPane from "@/components/GlassPane";
+import Sidebar from "@/components/Sidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -12,6 +13,7 @@ export default function DashboardRootLayout({ children }: Props) {
       <head />
       <body className="h-screen w-screen candy-mesh p-6">
         <GlassPane className={"w-full h-full flex items-center justify-center"}>
+          <Sidebar />
           {children}
         </GlassPane>
       </body>
